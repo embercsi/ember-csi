2e20540aafe610b362778b767c378a727bd185b9
Gorka Eguileor <geguileo@redhat.com>
2018-06-27 13:04:14 +0200

    Support Docker Hub automated builds
    Move the Python files copied in the Docker build command for master to
    its own directory to workaround a possible bug in Docker hub's automated
    build system.
    
    Also move the docker files to the root directory, otherwise the
    automated build won't be able to copy any files since they are on the
    parent's directory.
    
    Related: docker/hub-feedback#811

--- tools/build-docker.sh
+++ tools/build-docker.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 cl_version=`grep 'current_version =' setup.cfg | sed 's/current_version = '//`
-docker build -t akrog/cinderlib-csi:v$cl_version -t akrog/cinderlib-csi:latest -f dockerfile/Dockerfile .
+docker build -t akrog/cinderlib-csi:v$cl_version -t akrog/cinderlib-csi:latest -f Dockerfile .
