c1d7920a988f9f2e01dab85c09e9cc3ece7d3628
Liora Milbaum <lmilbaum@redhat.com>
2018-11-07 13:03:06 +0200

    Dockerfile optimization Verify rpm packages installed pip packages installation use requirements file

--- automation/bootstrap.sh
+++ automation/bootstrap.sh
@@ -4,5 +4,8 @@ set -e
 
 PKGS="epel-release git gcc python-devel lvm2 docker"
 yum install -y $PKGS
+rpm -V --nomode $PKGS
 systemctl start docker.service
 yum install -y python2-pip
+rpm -V python2-pip
+pip install --upgrade -r /vagrant/automation/requirements.txt
