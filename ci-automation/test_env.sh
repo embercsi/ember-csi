1493b17179029e3de5a46a65303ddce4a117a71e
Liora Milbaum <lmilbaum@redhat.com>
2018-11-13 13:54:55 +0200

    Barak Reviews

--- ci-automation/test_env.sh
+++ ci-automation/test_env.sh
@@ -4,11 +4,12 @@
 command=$1
 
 if [[ "$command" = "start" ]]; then
-	docker build -t ember-csi .
+	docker build -t centos-test-env .
 	docker run -d --privileged -d -e 'container=docker' \
 		-v /sys/fs/cgroup:/sys/fs/cgroup:rw -v $(pwd)/../:/root/ember-csi \
-		--name ember-csi-test ember-csi
-	docker exec -i ember-csi-test /root/ember-csi/ci-automation/container_up.sh
+		--name centos-test-env centos-test-env
+	docker exec -i centos-test-env /root/ember-csi/ci-automation/container_up.sh
+	sudo rm -rf .vagrant/machines
 elif [[ "$command" = "end" ]]; then
-	docker rm -f $(docker ps -aq)
+	docker rm -f centos-test-env
 fi
