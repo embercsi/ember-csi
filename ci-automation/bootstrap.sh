2c386d8375bff94cae5ef13ea2784a7b8edba83e
Liora Milbaum <liora@lmb.co.il>
2019-01-21 12:20:01 +0200

    Using prebuilt generic image  (#49)
    Switched to prebuilt quay image + fix workspace path
    Created a generic image stored in quay.
    No need to create ImageStream and build it.
    Fixed a bug. The VM sources path was hard coded. Didn't work in other jobs/branches.

--- ci-automation/bootstrap.sh
+++ ci-automation/bootstrap.sh
@@ -11,6 +11,7 @@ PKGS=(git gcc python-devel lvm2 docker python2-pip e2fsprogs iscsi-initiator-uti
 yum install -y "${PKGS[@]}"
 rpm -V --nomode "${PKGS[@]}"
 systemctl start docker.service iscsid.service
+pip install --upgrade setuptools
 pip install --upgrade -r $home/requirements.txt
 pip install -r $home/requirements_dev.txt
 pip install -e $home/..
