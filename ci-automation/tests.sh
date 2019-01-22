7cb9ee2d4e364172b8d39d80504405ec24318dc0
Liora Milbaum <lmilbaum@redhat.com>
2019-01-22 15:42:36 +0200

    cleanup when tests fails

--- ci-automation/tests.sh
+++ ci-automation/tests.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 set -e
 
+cleanup()
+{
+  echo "Cleaning up"
+  pkill -6 ember-csi
+  echo "Done cleanup... quitting"
+}
+
+trap cleanup EXIT
+
 home=$( dirname "${BASH_SOURCE[0]}" )
 sudo $home/bootstrap.sh
 
@@ -12,5 +21,3 @@ sudo travis-scripts/setup-lvm.sh
 echo "-------------------- bm-lvm     --------------------"
 make centos-bm-lvm
 echo "-------------------- done       --------------------"
-
-pkill -6 ember-csi
