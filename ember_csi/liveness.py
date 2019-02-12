e2bda19587bbedcf7007ce87930e5748b501d861
Gorka Eguileor <geguileo@redhat.com>
2019-02-12 13:02:09 +0100

    Add liveness command
    We cannot use Kubernetes CSI livenessprobe side container since most, if
    not all, the Ember-CSI controller and Node deployments require sharing
    the network with the host, so we cannot just bind the HTTP service to a
    port.
    
    To add support for Kubernetes liveness probes we add a simple command
    line tool called ember-liveness that can be used in our ember-csi
    containers (Controller and Nodes) like this:
    
       livenessProbe:
         exec:
           command:
           - ember-liveness
         initialDelaySeconds: 120
         periodSeconds: 90
         timeoutSeconds: 60

---
+++ ember_csi/liveness.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# Copyright (c) 2019, Red Hat, Inc.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from __future__ import absolute_import
+import importlib
+import sys
+
+import grpc
+
+from ember_csi import config
+
+
+def main():
+    conf = config.CONF
+    conf.validate()
+
+    prefix = 'ember_csi.v%s.csi_pb2' % conf.CSI_SPEC.replace('.', '_')
+    pb2 = importlib.import_module(prefix)
+    pb2_grpc = importlib.import_module(prefix + '_grpc')
+
+    try:
+        with grpc.insecure_channel(conf.ENDPOINT) as channel:
+            stub = pb2_grpc.IdentityStub(channel)
+            response = stub.Probe(pb2.ProbeRequest())
+    except grpc.RpcError as exc:
+        sys.stderr.write('Error calling CSI Probe: %s\n' % exc)
+        sys.exit(2)
+
+    if conf.CSI_SPEC != '0.2.0' and not response.ready.value:
+        sys.stderr.write('CSI Probe reports not ready')
+        sys.exit(1)
+
+    sys.stdout.write('CSI Probe reports ready')
+
+
+if __name__ == '__main__':
+    main()
