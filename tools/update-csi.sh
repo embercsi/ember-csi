39cac44e11830a8cf07636dfe8711011805c0208
Gorka Eguileor <geguileo@redhat.com>
2020-04-16 13:29:00 +0200

    Add py2 and py3 csi_pb2 files
    Now that we support Python 2 and 3 it would be nice to have independent
    csi_pb2.py and csi_pb2_grpc.py files for each Python version.
    
    This patch adds the necessary mechanism to have this and support the
    installation without having the unnecessary files included.
    
    While developing we can manually change the files using:
    
      python setup.py csi_proto

--- tools/update-csi.sh
+++ tools/update-csi.sh
@@ -6,8 +6,8 @@
 curl -o csi.proto https://raw.githubusercontent.com/container-storage-interface/spec/master/csi.proto
 
 if [[ ! -v VIRTUAL_ENV ]]; then
-    tox -epy27 --notest
-    . .tox/py27/bin/activate
+    tox -epy36 --notest
+    . .tox/py36/bin/activate
 fi
 
 python -m grpc_tools.protoc --proto_path=. --grpc_python_out=./ember_csi --python_out=./ember_csi csi.proto
