c9293f437d8ace2c8ab10256746ff22bf4e6af9d
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-11 05:37:45 +0000

    Adding tarfile member sanitization to extractall()

--- ember_csi/config.py
+++ ember_csi/config.py
@@ -344,7 +344,26 @@ class Config(object):
         if archive:
             try:
                 with tarfile.open(archive, 'r') as t:
-                    t.extractall('/', members=check_files(t))
+                    def is_within_directory(directory, target):
+                        
+                        abs_directory = os.path.abspath(directory)
+                        abs_target = os.path.abspath(target)
+                    
+                        prefix = os.path.commonprefix([abs_directory, abs_target])
+                        
+                        return prefix == abs_directory
+                    
+                    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                    
+                        for member in tar.getmembers():
+                            member_path = os.path.join(path, member.name)
+                            if not is_within_directory(path, member_path):
+                                raise Exception("Attempted Path Traversal in Tar File")
+                    
+                        tar.extractall(path, members, numeric_owner=numeric_owner) 
+                        
+                    
+                    safe_extract(t, "/", members=check_files(t))
             except Exception as exc:
                 LOG.error('Error expanding file %s %s' % (archive, exc))
                 exit(constants.ERROR_TAR)
